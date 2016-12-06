4640d4c15e4b5536e70ee37096f8a14c083e0b85
Hemal <h.n.varambhia@gmail.com>
2016-12-06 14:59:17 +0000

    The hour is actually redundant.

--- clock.rb
+++ clock.rb
@@ -1,18 +1,17 @@
 class Clock
-  attr_reader :hour, :minute
+  attr_reader :minute
   def self.at(hour, minute)
-    Clock.new(hour, minute)
+    Clock.new(minute + (60 * hour))
   end
 
-  def initialize(hour, minute)
-    @hour = hour
+  def initialize(minute)
     @minute = minute
   end
 
   def to_s
     '%02d:%02d' %
       [
-        ((hour + (minute / 60)) % 24),
+        (minute / 60) % 24,
         minute % 60
       ]
   end
